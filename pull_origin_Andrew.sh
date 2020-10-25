2f0858c3da7ca796b04e01507e9758456e407dff
Gabriel Shepherd <gabriel.shepherd@outlook.com>
2020-10-25 15:05:56 -0500

    we don't need the bash scripts anymore to automate pushing and pulling because Dawson is a wizard, Harry.

--- pull_origin_Andrew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script will pull down code from the origin from master
-# move from Desktop to home
-cd ..
-# move from home to Documents (repo is here
-cd Documents/Andrew/
-# fetch origin and reset hard
-git pull origin Andrew
-git reset --hard origin/Andrew
--- pull_origin_Gabe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# this script will pull down code from the origin from master
-# move from Desktop to home
-cd ..
-# move from home to Documents (repo is here
-cd Documents/Gabe/
-# fetch origin and reset hard
-git pull origin Gabe
-git reset --hard origin/Gabe
