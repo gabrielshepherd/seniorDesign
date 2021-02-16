07f515eb241498728e2d2a0b366762309a71b8a1
Andrew Elliott <kingelliott80@gmail.com>
2021-02-16 16:04:12 -0600

    test

--- LEDs/led_test.py
+++ LEDs/led_test.py
@@ -78,6 +78,13 @@ while True:
     # pixels.fill((0, 0, 255, 0))
     pixels.show()
     time.sleep(5)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 0, 0, 255))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 0, 255, 0))
+    pixels.show()
+    time.sleep(5)
  
     #rainbow_cycle(0.001)  # rainbow cycle with 1ms delay per step
  
