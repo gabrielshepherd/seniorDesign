7cbf8a1478363c430d096a3efb0a2e53f9c500de
gabrielshepherd <63371956+gabrielshepherd@users.noreply.github.com>
2021-02-24 19:45:14 -0600

    test script

---
+++ app_wip.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+import multiprocessing as mp
+import time
+import queue
+
+app = Flask(__name__)
+running_flag = mp.Value("i", 1)
+q = mp.Queue()
+
+def function_call(location, t0):
+    t1 = time.time()
+    dt = t1 - t0
+    
+    print(location)
+    return
+
+@app.route('/')
+def connect():
+    return "connection confirmed"
+
+@app.route('/transmit')
+def receive_data():
+    t0 = time.time()
+    location = request.json['location']
+    function_call(location, t0)
+    return "data received"
