0172a558144364f9ec5c3db89731425959d1a9bb
gabrielshepherd <63371956+gabrielshepherd@users.noreply.github.com>
2021-02-28 21:00:35 -0600

    ready update for Andrew
    Dawson and I added a thread to serve as a count down timer for the idle mode "rainbow" functionality in app.py

--- app.py
+++ app.py
@@ -1,17 +1,67 @@
-from flask import Flask, request
+from flask import Flask, request, after_this_request
+from threading import Thread, Event, Timer
+import time
+
+def idle_mode():
+    # call rainbow function (or some fraction of it)
+    pass
+
+
+class MyThread(Thread):
+    WAIT_TIME = 1000                        # in 10s of milliseconds
+
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        # Sleep for [time period]
+        for i in range(MyThread.WAIT_TIME):
+            if self.stopped.wait(0.01):
+                return
+        # Do the animation
+        while not self.stopped.wait(0.01):
+            # print(f"my thread ({count})")
+            idle_mode()
+            # call a function
+
+
+# init flask app
 app = Flask(__name__)
+
 
 def function_call(location):
     # Andrew, this is where your stuff should go
     print(location)
-    return
+
 
 @app.route('/')
 def connect():
     return "connection confirmed"
 
+
 @app.route('/transmit')
 def receive_data():
+    global idle_thread
+
+    # stop the idle animation thread
+    stopFlag.set()
+    idle_thread.join()
+
+    # do the actual request handling stuff with the LEDs
     location = request.json['location']
     function_call(location)
-    return "data received"
+
+    # Restart the idle animation thread
+    stopFlag.clear()
+    idle_thread = MyThread(stopFlag)
+    idle_thread.start()
+
+    return "thank you for sending " + location
+
+
+if __name__ == '__main__':
+    stopFlag = Event()
+    idle_thread = MyThread(stopFlag)
+    idle_thread.start()
+    app.run(debug=False, host='0.0.0.0')
--- send.py
+++ send.py
@@ -2,7 +2,8 @@ import requests
 
 def data_transmit(location):        # location is a string
     print("we are here")
-    response = requests.get('http://172.16.0.1:5000/transmit', json={'location': str(location)})
+    # response = requests.get('http://172.16.0.1:5000/transmit', json={'location': str(location)})
+    response = requests.get('http://localhost:5000/transmit', json={'location': str(location)})
     print(response.status_code)
     print(response.text)
     return "data sent"
