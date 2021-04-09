b7719f0283d893c12927274bbc0e9751a17a3e13
Will-Halfmann <William.halfmann@ndsu.edu>
2021-04-09 13:49:17 -0500

    simple exact string location

--- excel.py
+++ excel.py
@@ -1,6 +1,8 @@
 #import openpyxl 
 import xlrd 
 import xlwt 
+resistor = "IC LDO REG 500mA 1.2 V, TO220-3, POS FIXED, 2.1-6V IN,1.2OUT"
+location = ""
 count = 0
 workbook = xlrd.open_workbook('ECEparts.xls')
 
@@ -15,7 +17,7 @@ print (first_sheet.row_values(0))
 
 # Value of 1st row and 1st column
 cell = first_sheet.cell(26, 1).value
-print (cell) 
+#print (cell) 
 #if sheet.cell(0, 0).value == xlrd.empty_cell.value:
     # Do something
 
@@ -24,16 +26,30 @@ cols = first_sheet.ncols
 #loop to parse entire sheet
 for row in range(rows):
    for col in range(cols):
-      cellA = first_sheet.cell(row, 1) 
-      locationdata[cellA] = {}
-      print(cellA.value)
+      cellB = first_sheet.cell(row, 1) 
+      cellE = first_sheet.cell(row, 4) 
+      #find a close match to the search
+
+      if cellE.value == resistor :
+         location = cellB.value
+         break
+      
+      locationdata[cellB] = {}
+      #print(cellB.value)
 
 
 for row in range(rows):
    for col in range(cols):
-      cellD = first_sheet.cell(row, 4) 
-      descriptdata[cellD] = {}
-      print(cellD.value)
+      cellE = first_sheet.cell(row, 4) 
+      descriptdata[cellE] = {}
+      #print(cellE.value)
+     
 
 
-#data[500]
+
+
+#print (descriptdata)
+#print (locationdata)
+print (location)
+
+#print (locationdata[1:3])
