9b03d0781317522efdad51e8cb2aca9f59b580f7
Andrew Elliott <kingelliott80@gmail.com>
2021-04-14 17:02:39 -0500

    test

--- LEDs/led_location.py
+++ LEDs/led_location.py
@@ -55,9 +55,7 @@ def rainbow_cycle(wait, j):
     # for j in range(255):
     for i in range(335):
         pixel_index = (i * 256 // num_pixels) + j
-        # pixels[i] = wheel(pixel_index & 255)
-        # pixels[i] = wheel(pixel_index & 255)
-        pixels.fill(wheel(pixel_index & 255))
+        pixels[i] = (wheel(pixel_index & 255))
 
     pixels.show()
     time.sleep(wait)
