f43e9b546a48b02e52f18e4a53d32cdbe3f168d0
Dylan <me@me.com>
2021-04-21 11:22:20 -0900

    Sizing fixes.

--- MainGUICode/send.py
+++ MainGUICode/send.py
@@ -6,9 +6,9 @@ def data_transmit(location):        # location is a string
     print("we are here")
     try :
         print(location)
-        #response = requests.get('http://172.16.0.1:5000/transmit', json={'location': str(location)})
+        response = requests.get('http://172.16.0.1:5000/transmit', json={'location': str(location)})
     except:
             messagebox.showerror("Connection Error", "Check Connection")
-    #print(response.status_code)
-    #print(response.text)
+    print(response.status_code)
+    print(response.text)
     return "data sent"
