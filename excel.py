aa2ee78fe397f2aa8907c6fd4865bd14b83e949b
Will-Halfmann <William.halfmann@ndsu.edu>
2021-04-21 13:07:39 -0500

    Added difflib functions to get closest string. needs testing on the py tho

--- excel.py
+++ excel.py
@@ -1,15 +1,25 @@
+#This file is for communitcation with the Main GUI specific search page, It takes a string input by the user and searches the NDSU ece parts inventory display excel sheet for the location
 #import openpyxl 
 import xlrd 
 import xlwt 
+import difflib
 
+#Currently working on getting difflib to find nearest match searches from the user so instead of needing an exact string the user can type in what they want.
+#Future Updates could include making the get closest string functionality more efficent and effective.
+
+
+#  https://docs.python.org/3/library/difflib.html#difflib.get_close_matches
+
+#The above link is documentation on the difflib library that is being used to get closes strings on the search code.
 def search(input):
-   #input = "IC LDO REG 500mA 1.2 V, TO220-3, POS FIXED, 2.1-6V IN,1.2OUT"
+   #input = "IC LDO REG 500mA 1.2 V, TO220-3, POS FIXED, 2.1-6V IN,1.2OUT" <==== test location
    location = ""
    count = 0
-   workbook = xlrd.open_workbook('StorageRoomData.xls')
+   workbook = xlrd.open_workbook('StorageRoomData.xls')          # <===== if the sheet name ever changes the file name needs to change here as well otherwise the code will not initalize
 
    first_sheet = workbook.sheet_by_index(0)
 
+   #some useful arrays that are initalized to create dictionarys to parse if needed
    descriptdata = {}
    locationdata = {}
 
@@ -25,6 +35,48 @@ def search(input):
 
    rows = first_sheet.nrows
    cols = first_sheet.ncols
+  
+   for row in range(rows):
+      for col in range(cols):
+         cellE = first_sheet.cell(row, 4) 
+         descriptdata[cellE] = {}
+         #print(cellE.value)
+         
+         #this is the difflib get closest match statements. Ive taken strings from the quick search buttons to add into here to try and help the user with the searches.
+
+         #I need to test how this looks rigt now because if these can get working all I need to do is to take the cellE.data input and before parsing for location send it 
+         #through the get closest match to find an appropriate string from the sheet. Then that string can be sent as the cellE data input to look for location.
+         #Testing from home is hard but these are example strings
+         dResistor = difflib.get_close_matches('Resistor', descriptdata, 5, 0.5)
+         
+         dOpamp = difflib.get_close_matches('Op amp', descriptdata, 5, 0.5)
+         
+         dVReg = difflib.get_close_matches('V Reg', descriptdata, 5, 0.5)
+         
+         dInductor = difflib.get_close_matches('Inductor', descriptdata, 5, 0.5)
+         
+         dDiode = difflib.get_close_matches('Diode', descriptdata, 5, 0.5)
+         
+         dCapacitor = difflib.get_close_matches('Capacitor', descriptdata, 5, 0.5)
+         
+         dZener = difflib.get_close_matches('Zener', descriptdata, 5, 0.5)
+         
+         dCmosLogic = difflib.get_close_matches('Cmos Logic', descriptdata, 5, 0.5)
+         
+         dTTLLogic = difflib.get_close_matches('TTL Logic', descriptdata, 5, 0.5)
+         
+         dMultiplexer = difflib.get_close_matches('Multiplexer', descriptdata, 5, 0.5)
+         
+         dFlipFlop = difflib.get_close_matches('Flip-Flop', descriptdata, 5, 0.5)
+         
+         dCrystal = difflib.get_close_matches('Crystal', descriptdata, 5, 0.5)
+         
+         dMicrocontrollers = difflib.get_close_matches('Microcontrollers', descriptdata, 5, 0.5)
+
+         #not sure if dinput will work in order to test input strings
+         dinput = difflib.get_close_matches(input , descriptdata, 5, 0.5)
+  
+  
    #loop to parse entire sheet
    for row in range(rows):
       for col in range(cols):
@@ -32,19 +84,66 @@ def search(input):
          cellE = first_sheet.cell(row, 4) 
          #find a close match to the search
 
+         #This statement requires Exact string from the input to be able to search the spreadsheet
          if cellE.value == input :
             location = cellB.value
             break
+
+         if cellE.value == dFlipFlop :
+            location = cellB.value
+            break
+
+         if cellE.value == dMicrocontrollers :
+            location = cellB.value
+            break
+
+         if cellE.value == dMultiplexer:
+            location = cellB.value
+            break
+
+         if cellE.value == dOpamp :
+            location = cellB.value
+            break
+
+         if cellE.value == dResistor :
+            location = cellB.value
+            break
+
+         if cellE.value == dVReg :
+            location = cellB.value
+            break
+
+         if cellE.value == dInductor :
+            location = cellB.value
+            break
+
+         if cellE.value == dDiode :
+            location = cellB.value
+            break
+
+         if cellE.value == dCapacitor :
+            location = cellB.value
+            break
+
+         if cellE.value == dZener :
+            location = cellB.value
+            break
+
+         if cellE.value == dCmosLogic :
+            location = cellB.value
+            break
+
+         if cellE.value == dTTLLogic :
+            location = cellB.value
+            break
+
+         if cellE.value == dCrystal :
+            location = cellB.value
+            break
    
+
          locationdata[cellB] = {}
          #print(cellB.value)
-
-   for row in range(rows):
-      for col in range(cols):
-         cellE = first_sheet.cell(row, 4) 
-         descriptdata[cellE] = {}
-         #print(cellE.value)
-      
 
    #0-59 is 1 60-120 is 2 121-180 is 3
 
