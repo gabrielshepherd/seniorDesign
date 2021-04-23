f762e8552d01b6360c12eb71da2d444c662fd2e5
Andrew Elliott <kingelliott80@gmail.com>
2021-04-23 12:58:42 -0500

    test

--- LEDs/led_animation.py
+++ LEDs/led_animation.py
@@ -186,11 +186,11 @@ def vertical_snake(stop):
 def bouncing_led():
     clear()
     randomColor = random.randint(1,7)
-    for i in range(1000):
+    for i in range(100):
         randomLED = random.randint(1,780)
         pixels[randomLED] = colors(randomColor, 255)
         pixels.show()
-        time.sleep(0.001)
+        time.sleep(0.01)
     clear()
 
     
