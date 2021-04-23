bdd8d7399e8530aeea0762299ab479dc564742d0
Andrew Elliott <kingelliott80@gmail.com>
2021-04-23 12:21:54 -0500

    test

--- LEDs/led_animation.py
+++ LEDs/led_animation.py
@@ -121,7 +121,7 @@ def random_box(stop):
             time.sleep(0.005)
     return
 
-snake = [(0,35,1),(335,395,1),(237,201,-1),(396,457,1),(73,343,1)]
+snake = [(0,35,1),(335,395,1),(237,201,-1),(396,457,1),(73,342,1)]
 
 def vertical_snake(stop):
     for i in range(2):
