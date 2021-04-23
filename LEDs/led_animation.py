b8620a629f5315c603f9b685d114f13c3f5caf98
Andrew Elliott <kingelliott80@gmail.com>
2021-04-23 10:52:00 -0500

    test

--- LEDs/led_animation.py
+++ LEDs/led_animation.py
@@ -130,9 +130,9 @@ def vertical_snake(stop):
         for x in range(start, end, increment):
             pixels[x] = (255,0,0)
             light +=1
-            if light > 8 and light < 35:
+            if light >= 8 and light < 35:
                 pixels[light - 8] = (0,0,0)
-            if light > 35 and light < 43:
+            if light >= 35 and light < 43:
                 light2 = light + 335
                 pixels[light - 8] = (0,0,0)
                 pixels[light2 - 8] = (0,0,0)
